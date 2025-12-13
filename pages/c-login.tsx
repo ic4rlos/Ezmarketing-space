5652a064e2ba5f04fa44f0104b1c1ae1097b5c11
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2025-12-12 22:46:09 -0300

    Update c-login.tsx

--- pages/c-login.tsx
+++ pages/c-login.tsx
@@ -1,53 +1,18 @@
 import * as React from "react";
-import { useRouter } from "next/router";
-import {
-  PageParamsProvider as PageParamsProvider__,
-} from "@plasmicapp/react-web/lib/host";
 import GlobalContextsProvider from "../components/plasmic/ez_marketing_platform/PlasmicGlobalContextsProvider";
 import { PlasmicLCLogin } from "../components/plasmic/ez_marketing_platform/PlasmicLCLogin";
-import { useCLogin } from "../lib/useCLogin";
 
 export default function CLogin() {
-  const router = useRouter();
-  const { login, loading, error } = useCLogin();
-
   return (
     <GlobalContextsProvider>
-      <PageParamsProvider__
-        route={router.pathname}
-        params={router.query}
-        query={router.query}
-      >
-        <PlasmicLCLogin
-          overrides={{
-            email: {
-              props: {
-                onChange: (e) => (window as any).__email = e.target.value,
-              },
-            },
-            password: {
-              props: {
-                onChange: (e) => (window as any).__password = e.target.value,
-              },
-            },
-            loginButton: {
-              props: {
-                disabled: loading,
-                onClick: () =>
-                  login(
-                    (window as any).__email,
-                    (window as any).__password
-                  ),
-              },
-            },
-            errorText: {
-              props: {
-                children: error ?? "",
-              },
-            },
-          }}
-        />
-      </PageParamsProvider__>
+      {/*
+        Opção A:
+        - REMOVIDO PageParamsProvider__
+        - ZERO wrappers extras
+        - O .root do Plasmic continua sendo a página
+        - Nenhuma lógica de Supabase aqui ainda
+      */}
+      <PlasmicLCLogin />
     </GlobalContextsProvider>
   );
 }

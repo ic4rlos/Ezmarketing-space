ada7bdca86ea535793bd5507e1ce5960ae568050
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-03 22:30:08 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,2 +1,11 @@
-@/styles/globals.css"; import type { AppProps } from "next/app"; import { PlasmicRootProvider } from "@plasmicapp/react-web";
-export default function MyApp({ Component, pageProps }: AppProps) { return ( <PlasmicRootProvider> <Component {...pageProps} /> </PlasmicRootProvider> ); }
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { PlasmicRootProvider } from "@plasmicapp/react-web";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <PlasmicRootProvider>
+      <Component {...pageProps} />
+    </PlasmicRootProvider>
+  );
+}

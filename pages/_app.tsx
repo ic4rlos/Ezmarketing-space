2a865b6bc3380131eefa60d84a118a3f6c4ffc72
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-09 16:38:46 -0300

    Wrap Component with AuthProvider in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import { AuthProvider } from "../contexts/AuthContext";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </PlasmicRootProvider>
   );
 }

b5529978fbd404be2b6353a791bf405c44f96e01
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-09 17:13:10 -0300

    Update import path for AuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
-import AuthProvider from "../contexts/AuthContext";
+import AuthProvider from "@/contexts/AuthContext";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (

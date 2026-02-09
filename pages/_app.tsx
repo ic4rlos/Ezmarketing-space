3ac2a8babee2d85465b7d264189afc5db8bf019c
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-09 17:24:55 -0300

    Import CAuthProvider using named import syntax

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
-import CAuthProvider from "../contexts/c-AuthContext";
+import { CAuthProvider } from "../contexts/c-AuthContext";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (

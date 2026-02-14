51134386ae515af925ac6568f576092d27ef06bb
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-13 22:00:11 -0300

    Update c-edit-profile.tsx

--- pages/c-edit-profile.tsx
+++ pages/c-edit-profile.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useCAuth } from "../contexts/c-AuthContext";
-import { getSupabaseClient } from "../lib/supabaseClient";
+import { getSupabaseC } from "../lib/c-supabaseClient";
 import { PlasmicCEditProfile } from "../components/plasmic/ez_marketing_platform/PlasmicCEditProfile";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
 
@@ -15,7 +15,7 @@ export default function CEditProfile() {
     async function loadCompany() {
       if (!user) return;
 
-      const supabase = getSupabaseClient();
+      const supabase = getSupabaseC();
 
       const { data, error } = await supabase
         .from("companies")

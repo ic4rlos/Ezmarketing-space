522e1685ae8688998d639a8d3ba625ff86ea6b3f
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-17 15:02:32 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,10 @@
 import type { AppProps } from "next/app";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
-import { useCAuth } from "../contexts/c-AuthContext";
 
-function AppInner({ Component, pageProps }: AppProps) {
-  const { loading } = useCAuth();
-
-  // ⛔ NÃO renderiza Plasmic enquanto auth não existe
-  if (loading) {
-    return null;
-  }
-
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider>
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
-}
-
-export default function MyApp(props: AppProps) {
-  return <AppInner {...props} />;
 }

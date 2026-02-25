b9223423c123e784db5db6f7684cae0a13798533
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-25 14:18:59 -0300

    Create CropUpload.tsx

---
+++ components/CropUpload.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ImgCrop from 'antd-img-crop';
+import { Upload } from 'antd';
+import { UploadFile } from 'antd/lib/upload/interface';
+
+export interface CropUploadProps {
+  className?: string;
+  accept?: string;
+  onChange?: (file: UploadFile) => void;
+  children?: React.ReactNode;
+}
+
+export default function CropUpload({
+  className,
+  accept = "image/*",
+  onChange,
+  children,
+  ...props
+}: CropUploadProps) {
+  return (
+    <ImgCrop
+      showGrid
+      showReset
+      rotationSlider
+      cropShape="round"
+      modalOk="Recortar"
+      modalCancel="Cancelar"
+      resetText="Restablecer"
+    >
+      <Upload
+        className={className}
+        accept={accept}
+        multiple={false}
+        showUploadList={false}
+        onChange={({ file }) => {
+          onChange?.(file);
+        }}
+        {...props}
+      >
+        {children}
+      </Upload>
+    </ImgCrop>
+  );
+}

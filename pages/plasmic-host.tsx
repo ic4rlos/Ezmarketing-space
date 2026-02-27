8ebe0a1339f18a5d08761017672edfdb6aa0335f
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-27 14:22:38 -0300

    Update plasmic-host.tsx

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,18 +1,33 @@
-import * as React from 'react';
-import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/host';
-import CropUpload from '../components/CropUpload';
+import * as React from "react";
+import {
+  PlasmicCanvasHost,
+  registerComponent,
+} from "@plasmicapp/react-web/lib/host";
+import CropUpload from "../components/CropUpload";
 
 registerComponent(CropUpload, {
   name: "CropUpload",
   props: {
     onChange: {
-      type: "eventHandler"
+      type: "eventHandler",
+      argTypes: [
+        {
+          name: "file",
+          type: "object",
+        },
+      ],
     },
     accept: {
       type: "string",
-      defaultValue: "image/*"
-    }
-  }
+      defaultValue: "image/*",
+    },
+    className: {
+      type: "class",
+    },
+    children: {
+      type: "slot",
+    },
+  },
 });
 
 export default function PlasmicHost() {

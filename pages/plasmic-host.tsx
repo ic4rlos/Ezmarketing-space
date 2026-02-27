18c8903a85880caa05dbe63fec2e004095cf82e4
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-27 14:19:06 -0300

    Update plasmic-host.tsx

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/host';
+import CropUpload from '../components/CropUpload';
 
-// You can register any code components that you want to use here; see
-// https://docs.plasmic.app/learn/code-components-ref/
-// And configure your Plasmic project to use the host url pointing at
-// the /plasmic-host page of your nextjs app (for example,
-// http://localhost:3000/plasmic-host).  See
-// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
-
-// registerComponent(...)
+registerComponent(CropUpload, {
+  name: "CropUpload",
+  props: {
+    onChange: {
+      type: "eventHandler"
+    },
+    accept: {
+      type: "string",
+      defaultValue: "image/*"
+    }
+  }
+});
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;

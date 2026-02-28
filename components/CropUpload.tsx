ea71f344bb1a2394e2c1b9f6267348aaf1b199cb
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-02-27 21:10:36 -0300

    Update CropUpload.tsx

--- components/CropUpload.tsx
+++ components/CropUpload.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import ImgCrop from "antd-img-crop";
 import { Upload } from "antd";
 import { UploadFile } from "antd/lib/upload/interface";
 import { PlusOutlined } from "@ant-design/icons";
@@ -20,8 +19,6 @@ export default function CropUpload({
   children,
   ...props
 }: CropUploadProps) {
-  const hasChildren = React.Children.count(children) > 0;
-
   const defaultUI = (
     <div
       style={{
@@ -41,27 +38,17 @@ export default function CropUpload({
   );
 
   return (
-    <ImgCrop
-      showGrid
-      showReset
-      rotationSlider
-      cropShape="round"
-      modalOk="Recortar"
-      modalCancel="Cancelar"
-      resetText="Restablecer"
+    <Upload
+      className={className}
+      accept={accept}
+      multiple={false}
+      showUploadList={false}
+      onChange={({ file }) => {
+        onChange?.(file);
+      }}
+      {...props}
     >
-      <Upload
-        className={className}
-        accept={accept}
-        multiple={false}
-        showUploadList={false}
-        onChange={({ file }) => {
-          onChange?.(file);
-        }}
-        {...props}
-      >
-        {hasChildren ? children : defaultUI}
-      </Upload>
-    </ImgCrop>
+      {children || defaultUI}
+    </Upload>
   );
 }
